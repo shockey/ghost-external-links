844800d7dee7cd001f16115f13e23ca38dd79883
Kyle Shockey <kyleshockey1@gmail.com>
2015-02-18 12:20:24 -0600

    Added failover warning to jQuery check

--- ghost-external-links.js
+++ ghost-external-links.js
@@ -1,4 +1,4 @@
-window.jQuery || document.write("<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'>\x3C/script>");
+window.jQuery || document.write("<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'>\x3C/script>") || console.warn("Ghost External Links was unable to detect or add jQuery to the page.");
 
 $(document).ready(function() {
    $("a[href^=http]").each(function(){
