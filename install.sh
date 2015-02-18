63185fb574ea8c0d8d2577c7104ce779006ff761
Kyle Shockey <kyleshockey1@gmail.com>
2015-02-18 00:49:26 -0600

    localized variables, converted to camelCase, laid out execution of script, added check for js source, other changes

--- install.sh
+++ install.sh
@@ -1,35 +1,49 @@
-parse
-
 checkdirectory () 
 {
 if [ ! -d ./core ]; then
-	echo -e "Exiting: current directory is not a Ghost root"
+	echo -e "Exiting: current directory is not a Ghost root."
+	echo -e "Tip: Leave this script where it is, but call it from the root of your Ghost installation."
 	exit 1
 else
-	echo "Directory check: [OK]"
+	echo -e "Directory check: [OK]"
+fi;
+if [ ! -d $(dirname $0)/ghost-external-links.js ]; then
+	echo -e "Exiting: could not find ghost-external-links.js in the script's directory."
+	exit
 fi;
 }
 
-enumerate_themes () 
+enumerateThemes () 
 {
-dirs=();
+local dirs=();
 cd ./content/themes
 for file in ./*; do
-	dirs=(${dirs[@]} $file)
+	local dirs=(${dirs[@]} $file)
 done
 cd ../../
+echo -e 
 }
-
-
-
 
 # make the magic happen
 
 checkdirectory
-enumerate_themes
 
+echo -e "Welcome to Ghost External Links!"
+echo "Please choose an action, type 'install' or 'remove': "
+read action
+echo
 
-
+if [ $action = "install" ]; then
+	enumerateThemes
+	# collectWhitelist # prompt the user for a whitelist of domains
+	# stageSource # make a temporary derivative of the original source with the new whitelist injected
+	# installScript # copy the staged code into the theme directories
+	# injectReferences # inject references to our .js into themes
+elif [ $action = "remove" ]; then
+	enumerateThemes
+	removeScript
+	scrubReferences
+fi;
 
 
 #checks
